e7ff03c444678d3f8ffe5cdc96dbccc1322e673b
zhengnachuan <zhengnachuan@qq.com>
2022-09-01 23:09:56 +0800

    贪心

--- editor/cn/[714]买卖股票的最佳时机含手续费.java
+++ editor/cn/[714]买卖股票的最佳时机含手续费.java
@@ -54,11 +54,41 @@ public class BestTimeToBuyAndSellStockWithTransactionFee {
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int maxProfit(int[] prices, int fee) {
+    class Solution {
+        // dp
+        public int maxProfit2(int[] prices, int fee) {
+            // dp[i][0]第i天持有股票后的最多现金
+            // dp[i][1]第i天持有的最多现金
+            int[][] dp = new int[prices.length][2];
+            dp[0][0] = -prices[0];
+            dp[0][1] = 0;
+            for (int i = 1; i < prices.length; i++) {
+                // 第i天持股票所剩最多现金 = max(第i-1天持股票所剩现金, 第i-1天持现金-买第i天的股票)
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+                // 第i天持有最多现金 = max(第i-1天持有的最多现金，第i-1天持有股票的最多现金+第i天卖出股票)
+                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee);
+            }
+            return Math.max(dp[prices.length - 1][0], dp[prices.length - 1][1]);
+        }
+
+        // 贪心
+        public int maxProfit(int[] prices, int fee) {
+            int maybeBuy = prices[0] + fee;
+            int sum = 0;
+            for (int p : prices) {
+                // 找到递减最低买入点
+                if (maybeBuy > p + fee) {
+                    maybeBuy = p + fee;
+                    // 找到递增最高卖出点
+                } else if (maybeBuy < p) {
+                    sum += p - maybeBuy;
+                    maybeBuy = p;
+                }
+            }
+            return sum;
+        }
 
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
--- editor/cn/[738]单调递增的数字.java
+++ editor/cn/[738]单调递增的数字.java
@@ -50,11 +50,22 @@ public class MonotoneIncreasingDigits {
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int monotoneIncreasingDigits(int n) {
-
+    class Solution {
+        public int monotoneIncreasingDigits(int n) {
+            char[] chars = String.valueOf(n).toCharArray();
+            int start = chars.length;
+            for (int i = chars.length - 1; i > 0; i--) {
+                if (chars[i - 1] > chars[i]) {
+                    chars[i - 1]--;
+                    start = i;
+                }
+            }
+            for (int i = start; i < chars.length; i++) {
+                chars[i] = '9';
+            }
+            return Integer.parseInt(String.valueOf(chars));
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
